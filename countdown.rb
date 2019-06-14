879f8f20a2bda380e367a02d553d340ac93428c0
elegant-intelligence-1985 <cp.halpenny@gmail.com>
2019-06-14 03:44:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,8 @@
 def countdown(seconds)
   counter = 0
   while seconds > counter
-    puts "#{seconds} SECOND(S) !"
+    puts "#{seconds} SECOND(S)!"
     seconds -= 1
-
+  end
   puts "HAPPY NEW YEAR!"
-end
 end
